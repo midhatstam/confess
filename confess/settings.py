9bcec8f452112af6b0cfea9a8c4551163a253a26
Midhat <midhatstam@gmail.com>
2019-10-21 23:01:45 +0300

    circleci config update

--- confess/settings.py
+++ confess/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "ef+-3suc6+7wh%-n1hr71v83-5wvu7)dl8au#w9fe@4jd-af3#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost").split(" ")
 
 # Application definition
 
--- confession/tests.py
+++ confession/tests.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.utils import timezone
 
 from confession.models import Confession
 
 
+@override_settings(ALLOWED_HOSTS=["localhost", "127.0.0.1", "[::1]"])
 class ConfessionTest(TestCase):
     def setUp(self):
         Confession.objects.create(body='Testing....', item_meta_data_date=timezone.now(), admin_approved=1)
