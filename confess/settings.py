36ad5db4103923ebc74e4cd2525fe3d065bb1384
Midhat <midhat@dofo.com>
2019-10-22 13:15:19 +0300

    static update

--- confess/settings.py
+++ confess/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, "static")
+	os.path.join(BASE_DIR, "staticfiles")
 ]
 
 # STATIC_ROOT = '/static/'
