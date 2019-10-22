b2ede17c64d6d3b18664ffddf82b6f1941caacc9
Midhat <midhat@dofo.com>
2019-10-22 10:40:55 +0300

    STATIC_ROOT added

--- confess/settings.py
+++ confess/settings.py
@@ -141,6 +141,8 @@ STATICFILES_DIRS = [
 	'static/',
 ]
 
+STATIC_ROOT = '/static/'
+
 # REST_FRAMEWORK = {
 # 	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 # 	'PAGE_SIZE': 10
