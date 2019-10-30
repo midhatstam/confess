26ed66a6f3a0208995f514180322b0110ae94902
Midhat <midhat@dofo.com>
2019-10-30 16:36:54 +0300

    KON-13 Delete files #28k3wv[REVIEW]

--- .circleci/fabfile.py
+++ .circleci/fabfile.py
@@ -39,6 +39,8 @@ def deploy(ctx):
     conn = get_connection(ctx)
     if conn is None:
         sys.exit("Failed to get connection")
+    conn.run('cp project_settings.json /home/midhat/confess/.circleci/')
+    conn.run('cp .env.dev /home/midhat/confess/')
     with conn.cd(stage_settings().get('code_src_directory')):
         pull_git_repository(conn)
     venv_dir = stage_settings().get("venv_directory")
