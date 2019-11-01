e3a8b0d2cab536f2808d0c82394ed458dfcd123d
Midhat <midhat@dofo.com>
2019-11-01 12:32:16 +0300

    KON-16 django-environ #2ad3y1[REVIEW]

--- core/models.py
+++ core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class ItemMetaData(models.Model):
-	class Meta:
-		db_table = "item_meta_data"
-		abstract = True
+    class Meta:
+        db_table = "item_meta_data"
+        abstract = True
 
-	item_meta_data_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    item_meta_data_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
