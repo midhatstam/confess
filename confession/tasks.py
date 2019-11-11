589733dc41e5abab0f5c4f303b384416f02cd32a
Midhat <midhat@dofo.com>
2019-11-11 23:15:38 +0300

    KON-9 Task with most approved instances #2ag3zn[REVIEW]

--- confession/tasks.py
+++ confession/tasks.py
@@ -1,12 +1,26 @@
 import datetime
 
 from celery import task
+from django.db.models import Sum, When, Case, IntegerField
 
 from confession.models import Confession
+from confession.utils import slack_notify
 
 
 @task
 def set_publish_time():
     date_from = datetime.datetime.now() - datetime.timedelta(days=1)
-    instances: Confession = Confession.objects.filter(item_meta_data_date__gte=date_from)
+    instances = Confession.objects.filter(item_meta_data_date__gte=date_from).annotate(
+        approved_count=Sum(
+            Case(
+                When(confessionuserapprovement__vote=True, then=1),
+                When(confessionuserapprovement__vote=False, then=-1),
+                When(confessionuserapprovement=None, then=0),
+                output_field=IntegerField()
+            )
+        )
+    ).order_by('-approved_count')
 
+    for i in instances:
+        message = f'Confession with id: {i.id} and approved_count: {i.approved_count}'
+        slack_notify(message=message)
