6acf296d278b3ab16f568cfadadf9638563d498b
Midhat <midhat@dofo.com>
2019-12-05 11:34:23 +0300

    kon-25 confess publish date format #2pe0bh

--- confess/settings.py
+++ confess/settings.py
@@ -134,7 +134,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr-tr'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Europe/Istanbul'
