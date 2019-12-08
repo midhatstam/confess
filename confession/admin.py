f3fcf6a0ffdb849b36ee6dacde506fc001a59cd2
Midhat <midhat@dofo.com>
2019-12-08 19:06:35 +0300

    kon-27 remove confession css class #2pe0g9

--- confession/admin.py
+++ confession/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from confession.models import Confession
 
 
-class ConfessionAdmin(admin.ModelAdmin):
-	fields = '__all__'
-
-
-admin.site.register(Confession, ConfessionAdmin)
+admin.site.register(Confession)
